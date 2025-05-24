664842168d888e43260e19df2584b515f50c3797
tahmidtaqi <taqitahmid97@gmail.com>
2025-05-24 10:04:19 +0600

    added github yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://tahmidtaqi.github.io', // or custom domain
   base: '/taqiTahmidPortfolio/',
+  outDir: './dist',
 });
