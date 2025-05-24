ffe24936abc8511c9890521aab501ad57137b15c
tahmidtaqi <taqitahmid97@gmail.com>
2025-05-24 11:07:13 +0600

    upda\ting config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tahmidtaqi.github.io', // or custom domain
-  base: '/taqiTahmidPortfolio/',
+  //base: '/taqiTahmidPortfolio/',
   outDir: './dist',
-  
+
 });
